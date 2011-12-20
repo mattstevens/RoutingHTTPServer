7b4501005b119890c42ea4bec2505511df53400a
Matt Stevens <matt@alloysoft.com>
2011-12-19 20:54:51 -0700

    Add test for POST data

--- Example/RoutingHTTPServerTests/RoutingHTTPServerTests.m
+++ Example/RoutingHTTPServerTests/RoutingHTTPServerTests.m
@@ -9,6 +9,7 @@
 - (void)verifyRouteWithMethod:(NSString *)method path:(NSString *)path;
 - (void)verifyRouteNotFoundWithMethod:(NSString *)method path:(NSString *)path;
 - (void)handleSelectorRequest:(RouteRequest *)request withResponse:(RouteResponse *)response;
+- (void)verifyMethod:(NSString *)method path:(NSString *)path contentType:(NSString *)contentType inputString:(NSString *)inputString responseString:(NSString *)expectedResponseString;
 
 @end
 
@@ -49,6 +50,22 @@
 	[self verifyRouteNotFoundWithMethod:@"GET" path:@"/form"];
 }
 
+- (void)testPost {
+	NSError *error = nil;
+	if (![http start:&error]) {
+		STFail(@"HTTP server failed to start");
+	}
+
+	NSString *xmlString = @"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+		"<greenLevel>supergreen</greenLevel>";
+
+	[self verifyMethod:@"POST"
+				  path:@"/xml"
+		   contentType:@"text/xml"
+		   inputString:xmlString
+		responseString:@"supergreen"];
+}
+
 - (void)setupRoutes {
 	[http get:@"/hello" withBlock:^(RouteRequest *request, RouteResponse *response) {
 		[response respondWithString:@"/hello"];
@@ -85,6 +102,22 @@
 	}];
 
 	[http handleMethod:@"GET" withPath:@"/selector" target:self selector:@selector(handleSelectorRequest:withResponse:)];
+
+	[http post:@"/xml" withBlock:^(RouteRequest *request, RouteResponse *response) {
+		NSData *bodyData = [request body];
+		NSString *xml = [[[NSString alloc] initWithBytes:[bodyData bytes] length:[bodyData length] encoding:NSUTF8StringEncoding] autorelease];
+
+		// Green?
+		NSRange tagRange = [xml rangeOfString:@"<greenLevel>"];
+		if (tagRange.location != NSNotFound) {
+			NSUInteger start = tagRange.location + tagRange.length;
+			NSUInteger end = [xml rangeOfString:@"<" options:0 range:NSMakeRange(start, [xml length] - start)].location;
+			if (end != NSNotFound) {
+				NSString *greenLevel = [xml substringWithRange:NSMakeRange(start, end - start)];
+				[response respondWithString:greenLevel];
+			}
+		}
+	}];
 }
 
 - (void)handleSelectorRequest:(RouteRequest *)request withResponse:(RouteResponse *)response {
@@ -112,6 +145,35 @@
 
 	response = [http routeMethod:method withPath:path parameters:params request:request connection:nil];
 	STAssertNil(response, @"Response should have been nil for %@ %@", method, path);
+}
+
+- (void)verifyMethod:(NSString *)method path:(NSString *)path contentType:(NSString *)contentType inputString:(NSString *)inputString responseString:(NSString *)expectedResponseString {
+	NSError *error = nil;
+	NSData *data = [inputString dataUsingEncoding:NSUTF8StringEncoding];
+
+	NSString *baseURLString = [NSString stringWithFormat:@"http://127.0.0.1:%d", [http listeningPort]];
+
+	NSString *urlString = [baseURLString stringByAppendingString:path];
+	NSURL *url = [NSURL URLWithString:urlString];
+
+	NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:url];
+	[request setHTTPMethod:method];
+	[request addValue:contentType forHTTPHeaderField:@"Content-Type"];
+	[request addValue:[NSString stringWithFormat:@"%ld", [data length]] forHTTPHeaderField:@"Content-Length"];
+	[request setHTTPBody:data];
+
+	NSURLResponse *response;
+	NSHTTPURLResponse *httpResponse;
+	NSData *responseData = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+	STAssertNotNil(response, @"No response received for %@ %@", method, path);
+	STAssertNotNil(responseData, @"No response data received for %@ %@", method, path);
+	STAssertTrue([response isKindOfClass:[NSHTTPURLResponse class]], @"Response is not an NSHTTPURLResponse");
+
+	httpResponse = (NSHTTPURLResponse *)response;
+	STAssertEquals([httpResponse statusCode], 200L, @"Unexpected status code for %@ %@", method, path);
+
+	NSString *responseString = [[[NSString alloc] initWithBytes:[responseData bytes] length:[responseData length] encoding:NSUTF8StringEncoding] autorelease];
+	STAssertEqualObjects(responseString, expectedResponseString, @"Unexpected response for %@ %@", method, path);
 }
 
 @end
